7cea0f2dc416601a2f63ab98008094128fef4b9f
daimon_sewell <daimon.sewell@rbgapps.com>
2015-12-02 16:50:39 +0200

    Initial setup. Android application setup  with API 22

---
+++ src/com/CyberMelon/Calendar/CalendarMain.java
@@ -0,0 +1,15 @@
+package com.CyberMelon.Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalendarMain extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
